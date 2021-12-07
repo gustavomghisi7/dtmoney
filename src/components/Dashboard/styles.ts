abd6421c56122368f5486a8d92ece2d38becba1a
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-12-07 15:12:38 -0300

    api

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.main`
     max-width: 1120px;
+    height: 100%;
     margin: 0 auto;
     padding: 2.5rem 1rem;
+    background-color: var(--background);
 `;
