39e347d3b426659e618b5ec7b9945b47c68f2e4f
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2023-04-12 17:30:42 -0300

    Insert transaction table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
-    const transactions = useContext(TransactionsContext);
+    const { transactions } = useContext(TransactionsContext);
 
     return (
         <Container>
